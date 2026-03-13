0dc430459549598ed38626f45eafe33cc1556340
Likhitha Sri <gls251104@gmail.com>
2026-03-14 03:34:24 +0530

    Update rag_utils.py

--- utils/rag_utils.py
+++ utils/rag_utils.py
@@ -10,15 +10,20 @@ def load_documents():
     with open("documents/knowledge.txt", "r") as f:
         docs = f.readlines()
 
-    documents = docs
-    vectors = [get_embedding(doc) for doc in docs]
+    documents = [doc.strip() for doc in docs if doc.strip()]
+    vectors = [get_embedding(doc) for doc in documents]
 
 
 def retrieve_context(query):
+    global documents, vectors
+
+    if len(documents) == 0:
+        return "No documents loaded."
+
     query_vec = get_embedding(query)
 
     scores = [np.dot(query_vec, v) for v in vectors]
 
-    best_index = np.argmax(scores)
+    best_index = int(np.argmax(scores))
 
     return documents[best_index]
